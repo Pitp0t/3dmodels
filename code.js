1d2b16bd37f9218a243b4d44525f1956c2d30ee6
Gonzalo Iurman <gonpwn@gmail.com>
2022-05-20 04:23:55 -0300

    first commit

---
+++ code.js
@@ -0,0 +1,27 @@
+
+//========
+//Loader
+//========
+
+// const loaderWrapper = document.querySelector('.loader-wrapper');
+
+
+// window.onload= ()=>{
+//     loaderWrapper.classList.add('change');
+
+// }
+
+
+
+
+
+
+
+const visibilty = document.getElementById('material-symbols-outlined');
+
+
+visibilty.addEventListener('click',()=>{
+
+    visibilty.classList.toggle('color');
+
+})
